59a88393d03859596a9d7c459905c9b2b57ad97d
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-01-20 02:24:58 +0300

    repositories created

---
+++ src/main/java/com/example/graphqlexample/repository/FoodRepository.java
@@ -0,0 +1,9 @@
+package com.example.graphqlexample.repository;
+
+import com.example.graphqlexample.model.Food;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FoodRepository extends JpaRepository<Food, Long> {
+}
