bcbeb54b98c4c784e130a2c5f6db0f444694a639
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-01-20 02:22:19 +0300

    Initial commit

---
+++ src/main/java/com/example/graphqlexample/GraphqlexampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphqlexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphqlexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphqlexampleApplication.class, args);
+    }
+
+}
