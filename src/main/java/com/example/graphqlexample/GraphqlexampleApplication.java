5818a232aba09c11de24084c47487dd0367f5384
Gurkan UCAR <gurkanucar@trakya.edu.tr>
2022-01-25 13:25:50 +0300

    groovy implemented

--- src/main/java/com/example/graphqlexample/GraphqlexampleApplication.java
+++ src/main/java/com/example/graphqlexample/GraphqlexampleApplication.java
@@ -1,13 +1,44 @@
 package com.example.graphqlexample;
 
+import com.example.graphqlexample.model.Category;
+import com.example.graphqlexample.model.Food;
+import com.example.graphqlexample.repository.CategoryRepository;
+import com.example.graphqlexample.service.CategoryService;
+import com.example.graphqlexample.service.FoodService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.List;
+
 @SpringBootApplication
-public class GraphqlexampleApplication {
+public class GraphqlexampleApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(GraphqlexampleApplication.class, args);
     }
 
+    @Autowired
+    CategoryService categoryService;
+
+    @Autowired
+    FoodService foodService;
+
+    @Override
+    public void run(String... args) throws Exception {
+        categoryService.create(Category.builder().name("Dishes").build());
+        categoryService.create(Category.builder().name("Turkish Foods").build());
+        categoryService.create(Category.builder().name("Mexican Foods").build());
+        categoryService.create(Category.builder().name("Fishes").build());
+        categoryService.create(Category.builder().name("Soups").build());
+        categoryService.create(Category.builder().name("Deserts").build());
+
+        foodService.create(Food.builder().name("Cake").recipe("cake recipe").categories(List.of(Category.builder().id(6L).build())).build());
+        foodService.create(Food.builder().name("Kelle paca").recipe("soup recipe").categories(
+                List.of(Category.builder().id(2L).build(),
+                Category.builder().id(5L).build())).build());
+        foodService.create(Food.builder().name("Lahmacun").recipe("lahmacun recipe").categories(List.of(Category.builder().id(2L).build())).build());
+        foodService.create(Food.builder().name("Kebab").recipe("kebab").categories(List.of(Category.builder().id(2L).build())).build());
+    }
 }
--- src/main/java/com/example/graphqlexample/resolver/food/FoodQueryResolver.java
+++ src/main/java/com/example/graphqlexample/resolver/food/FoodQueryResolver.java
@@ -3,13 +3,20 @@ package com.example.graphqlexample.resolver.food;
 import com.example.graphqlexample.dto.FoodDTO;
 import com.example.graphqlexample.dto.converter.FoodConverter;
 import com.example.graphqlexample.service.FoodService;
+import com.github.javaparser.utils.Log;
 import graphql.kickstart.tools.GraphQLQueryResolver;
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+import groovy.util.logging.Log4j;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.logging.Logger;
 
+@Log4j
 @Component
 public class FoodQueryResolver implements GraphQLQueryResolver {
+
 
     private final FoodService foodService;
     private final FoodConverter converter;
@@ -24,7 +31,30 @@ public class FoodQueryResolver implements GraphQLQueryResolver {
         return converter.foodToDto(foodService.getAllFoods());
     }
 
-    public FoodDTO getFoodByID(Long id){
+    public List<FoodDTO> getAllFoods(String code) {
+     /*    Log.info("CODE: " + code.toString());
+       Log.info("CODE: " + code.toString());
+
+*/
+
+        var foods = converter.foodToDto(foodService.getAllFoods());
+
+        if (code == null || code.isEmpty()) {
+            return foods;
+        }
+        System.out.println("CODE: " + code.toString());
+        Binding binding = new Binding();
+        GroovyShell shell = new GroovyShell(binding);
+        shell.setVariable("foods", foods);
+
+        var data = shell.evaluate(code);
+        return (List<FoodDTO>) data;
+
+
+    }
+
+
+    public FoodDTO getFoodByID(Long id) {
         return converter.foodToDto(foodService.getByID(id));
     }
 
